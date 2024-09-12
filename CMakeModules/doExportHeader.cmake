57164993488992c4664cb90d09871f1096fb94e6
khushnoodq <khushno@gmail.com>
2024-09-13 03:22:31 +0530

    Added a CMakeScript to generate Export Macro

---
+++ CMakeModules/doExportHeader.cmake
@@ -0,0 +1,21 @@
+#________________________________________________________________________
+#
+# Author: Khushnood Qadir
+#________________________________________________________________________
+#
+
+function( do_export_header MODULE_NAME )
+    if( NOT MODULE_NAME )
+        message( FATAL_ERROR "MODULE_NAME argument is required" )
+    endif()
+
+    string(TOLOWER ${MODULE_NAME} MODULE_NAME_LOWER)
+    include(GenerateExportHeader)
+    generate_export_header( ${MODULE_NAME}
+        BASE_NAME ${MODULE_NAME}
+        EXPORT_MACRO_NAME Export_${MODULE_NAME}
+        EXPORT_FILE_NAME ${CMAKE_CURRENT_BINARY_DIR}/${MODULE_NAME_LOWER}mod.h
+    )
+
+    target_include_directories( ${MODULE_NAME} PUBLIC ${CMAKE_CURRENT_BINARY_DIR} )
+endfunction()
